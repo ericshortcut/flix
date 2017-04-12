6b5f5026a55a7e62ca477028382c4e72d8290035
eric_andrade <eric.oliveira.andrade@gmail.com>
2017-04-12 00:57:20 -0300

    Searching API

--- src/app.js
+++ src/app.js
@@ -1,8 +1,29 @@
 const app = require('./routes.js');
 const css = require('./scss/app.scss');
 
-app.controller('myCtrl', ($scope) => {
+app.controller('myCtrl', ( $scope ) => {
 
     $scope.helloWorld = "Hello world";
+
+})
+.controller('formCtrl', ( $scope, $http ) => {
+
+    $scope.pesquisar = ( movie ) => {
+
+        $scope.resultEmpty = true;
+        $scope.movies = [];
+        $scope.moviesError = "Searching ...";
+
+        $http
+          .get("https://omdbapi.com/?s="+movie)
+          .then(function(response) {
+              if(response.data.Response !== "False"){
+                  $scope.movies = response.data.Search;
+                  $scope.resultEmpty = false;
+              }
+          }, function(e){
+              $scope.moviesError = response.data.Error
+          });
+    };
 
 });
